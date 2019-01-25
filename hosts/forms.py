847374ba6409353961b2c61b1808d090a5f67047
Aytekin Aygün <aytekinaygun@gmail.com>
2019-01-25 06:08:07 +0300

    Host eklerken çoklu grup seçimi yapıldı.

--- hosts/forms.py
+++ hosts/forms.py
@@ -3,7 +3,10 @@ from .models import Hosts
 from host_groups.models import Host_Groups
 
 class HostsForm(forms.ModelForm):
-    hostgroups = forms.ModelMultipleChoiceField(queryset=Host_Groups.objects.all())
+    hostgroups = forms.ModelMultipleChoiceField(
+        widget=forms.SelectMultiple(attrs={'class': 'form-control select2'}),
+        queryset=Host_Groups.objects.values_list('hostgroup_name', flat=True), # flat True olmaz ise grup adı parantezli ve tırnak içinde geliyor.
+        )
 
     class Meta:
         model = Hosts
@@ -17,5 +20,4 @@ class HostsForm(forms.ModelForm):
             'host_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Örn: linux_sunucu'}),
             'alias': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Örn: Linux Sunucular'}),
             'address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Örn: Linux Sunucular'}),
-            'hostgroups': forms.Select(attrs={'class': 'form-control'}),
         }
