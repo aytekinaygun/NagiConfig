6178c489ebed5500e09c3e634413afed4bcd4359
Aytekin Aygün <aytekinaygun@gmail.com>
2019-01-31 16:55:43 +0300

    .

--- hosts/models.py
+++ hosts/models.py
@@ -4,12 +4,12 @@ from django.core.exceptions import ValidationError
 
 # Create your models here.
 class Hosts(models.Model):
-    host_name = models.CharField(max_length=50, verbose_name='Host Adı')
+    host_name = models.CharField(max_length=50, verbose_name='Host Adı', help_text="deneme")
     alias = models.CharField(max_length=150, verbose_name='Host Açıklaması')
     address = models.GenericIPAddressField(verbose_name='IP Adresi')
     parents = models.ManyToManyField('Hosts')
     hostgroups = models.ManyToManyField('host_groups.Host_Groups')
-    is_active = models.BooleanField(default=False, verbose_name=' Aktif edilsin mi?') 
+    is_active = models.BooleanField(default=False, verbose_name=' Aktif edilsin mi?')
 
     # Admin panelde listede grup adı görünür.
     def __str__(self):
