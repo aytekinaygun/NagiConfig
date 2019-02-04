fece41b29ee456eb70ebd9c737f6372cd3656660
Aytekin Aygün <aytekinaygun@gmail.com>
2019-02-04 04:04:24 +0300

    Dashboard; Toplamlar yapıldı. Host gruplarının toplam üye sayıları bulunacak...

--- home/views.py
+++ home/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from hosts.models import Hosts
+from host_groups.models import Host_Groups
+from services.models import Services
+from django.db.models import Count # group_by_host_groups
 
-# Create your views here.
+
 def home_index(request):
-    return render(request, 'home.html', {})
+    count_hosts          = Hosts.objects.count()
+    count_host_groups    = Host_Groups.objects.count()
+    count_services       = Services.objects.count()
+    group_by_host_groups = Hosts.objects.values('hostgroups').annotate(total=Count('hostgroups')).order_by('hostgroups')
+    hostgroups           = Host_Groups.objects.all()
+    # group_by_host_groups = Host_Groups.objects.values('hostgroup_name').annotate(total=Count('hostgroup_name')).order_by('hostgroup_name')
+    print(group_by_host_groups)
+    context = {
+        'count_hosts'          : count_hosts,
+        'count_host_groups'    : count_host_groups,
+        'count_services'       : count_services,
+        'group_by_host_groups' : group_by_host_groups,
+        'hostgroups'           : hostgroups,
+        }
+    return render(request, 'home.html', context)
