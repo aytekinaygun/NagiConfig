2c166cb70eb99dda4eb5a8638506d9a83faae9c0
Aytekin Aygün <aytekinaygun@gmail.com>
2019-02-18 04:14:23 +0300

    Servis komutu formu girdi kontrolü yapıldı.

--- services/forms.py
+++ services/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import ServiceCommand, Services
 from hosts.models import Hosts
+from django.core.exceptions import ValidationError
 
 class ServiceCommandForm(forms.ModelForm):
 
@@ -11,9 +12,24 @@ class ServiceCommandForm(forms.ModelForm):
             'check_command',
         ]
         widgets = {
-            'command_description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Örn: HTTP Web Servis Port:80'}),
+            'command_description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Örn: HTTP Web Servis Port 80'}),
             'check_command': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Örn: check_tcp!80'}),
         }
+
+    # Girilen değerler kısıtlara göre kontrol edilir.
+    def clean(self):
+        cd_error = 0
+        alias = self.cleaned_data.get('command_description')
+        for i in alias:
+            if ord(i) >= 128:
+                cd_error = 1
+        # Tire, boşluk, harf ve sayı dışında hata
+        if not alias.replace('-', '').replace(' ', '').isalnum():
+            cd_error = 1
+        if cd_error == 1:
+            raise ValidationError(('Servis komut açıklaması kurallara uygun değil!'), code='invalid')
+
+        return super(ServiceCommandForm, self).clean()
 
 
 # Servisler
