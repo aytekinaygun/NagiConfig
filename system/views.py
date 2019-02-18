a16b5aaa91e94cc193bc327ac4364819598023fc
Aytekin Aygün <aytekinaygun@gmail.com>
2019-02-18 04:50:50 +0300

    servis tanımında, eklenecek host_name yok ise (host var ama aktif değil ise) servis tanımı yapılmayacak. conf dosyasına bu servise ait tanım yazılmayacak.

--- system/views.py
+++ system/views.py
@@ -54,17 +54,22 @@ def nagios_restart(request):
     services = Services.objects.exclude(hosts__isnull=True) # host ilişkisi boş olmayanlar
     f = open('nc-services.cfg', 'w')
     for s in services:
-        f.write('define service{\n')
-        f.write('   service_description %s\n' % (s.service_description))
+        success = 1
+        define =  ('define service{\n')
+        define += ('   service_description %s\n' % (s.service_description))
         # Hosts
         h_list = ''
-        for h in s.hosts.all():
-            h_list = h_list + h.host_name + ' '
+        for h in s.hosts.filter(is_active=True):
+            h_list += h.host_name + ' '
         h_list = h_list.strip().replace(' ', ', ')
-        f.write('   host_name   %s\n' % (h_list))
+        if h_list == '':
+            success = 0
+        define += ('   host_name   %s\n' % (h_list))
         cmd = ServiceCommand.objects.get(id=s.service_description_id)
-        f.write('   check_command   %s\n' % (cmd.check_command))
-        f.write('}\n')
+        define += ('   check_command   %s\n' % (cmd.check_command))
+        define += ('}\n')
+        if success == 1:
+            f.write(define)
     f.close()
 
     messages.success(request, 'Nagios tekrar başlatıldı.', extra_tags='alert-success')
